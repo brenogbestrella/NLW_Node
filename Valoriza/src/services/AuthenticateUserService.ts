540f3ab65309bc1df9d9c9ccd4b6763495569ff5
Breno Estrella <brenogbestrella@gmail.com>
2021-06-26 17:16:47 -0300

    Commit aula 4

---
+++ Valoriza/src/services/AuthenticateUserService.ts
@@ -0,0 +1,46 @@
+import { getCustomRepository } from "typeorm";
+
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "37cba829af1cfd51fcd23ec2e13e592a",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
