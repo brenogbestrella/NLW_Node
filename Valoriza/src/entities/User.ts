bb052c8193d5c7fd455bcca729931394b3d8719e
Breno Estrella <brenogbestrella@gmail.com>
2021-06-27 14:50:12 -0300

    Commit aula 5

--- Valoriza/src/entities/User.ts
+++ Valoriza/src/entities/User.ts
@@ -5,6 +5,8 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from "typeorm";
+import { Exclude } from "class-transformer";
+
 import { v4 as uuid } from "uuid";
 
 @Entity("users")
@@ -21,6 +23,7 @@ class User {
   @Column()
   admin: boolean;
 
+  @Exclude()
   @Column()
   password: string;
 
